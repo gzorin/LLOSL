fce6fa80642eb0aff01c52f5a3525acf8e763698
Alex Betts <alex.betts@gmail.com>
2019-08-16 17:24:04 -0400

    Represent shader group parameters

--- include/llosl/ShaderGroup.h
+++ include/llosl/ShaderGroup.h
@@ -5,6 +5,9 @@
 #include <llvm/ADT/ilist_node.h>
 #include <llvm/IR/TrackingMDRef.h>
 
+#include <OpenImageIO/typedesc.h>
+#include <OpenImageIO/ustring.h>
+
 #include <memory>
 
 namespace llvm {
@@ -34,9 +37,45 @@ public:
 
     llvm::Type *globals_type() const { return d_globals_type; }
     llvm::Type *data_type() const { return d_data_type; }
+    llvm::Type *parameters_type() const { return d_parameters_type; }
 
     const llvm::Function *init_function() const;
     const llvm::Function *main_function() const;
+
+    class Parameter {
+    public:
+
+        unsigned index() const;
+        llvm::StringRef name() const;
+        llvm::Type *llvm_type() const;
+        OIIO::TypeDesc osl_type() const;
+
+    private:
+
+        friend class ShaderGroup;
+
+        Parameter(unsigned, const OIIO::ustring&, const OIIO::TypeDesc&, ShaderGroup *);
+        ~Parameter() = default;
+
+        ShaderGroup* d_parent = nullptr;
+
+        llvm::TypedTrackingMDRef<llvm::MDTuple> d_md;
+        unsigned d_md_index = 0;
+    };
+
+    using parameter_iterator = Parameter *;
+    using const_parameter_iterator = const Parameter *;
+
+    parameter_iterator arg_begin() { return d_parameters; }
+    const parameter_iterator arg_begin() const { return d_parameters; }
+
+    parameter_iterator arg_end() { return d_parameters + parameter_count(); }
+    const parameter_iterator arg_end() const { return d_parameters + parameter_count(); }
+
+    std::size_t parameter_count() const { return d_parameter_count; }
+
+    llvm::MDNode *metadata() { return d_md.get(); }
+    const llvm::MDNode *metadata() const { return d_md.get(); }
 
 private:
 
@@ -47,9 +86,14 @@ private:
     std::unique_ptr<llvm::Module> d_module;
     llvm::Type *d_globals_type = nullptr;
     llvm::Type *d_data_type = nullptr;
+    llvm::Type *d_parameters_type = nullptr;
+
+    std::size_t d_parameter_count = 0;
+    Parameter *d_parameters = nullptr;
 
     llvm::TypedTrackingMDRef<llvm::MDNode> d_md;
     llvm::TypedTrackingMDRef<llvm::ValueAsMetadata> d_init_function_md, d_main_function_md;
+    llvm::TypedTrackingMDRef<llvm::MDTuple> d_parameters_md;
 
     friend class BuilderImpl;
 };
--- lib/ShaderGroup.cpp
+++ lib/ShaderGroup.cpp
@@ -3,6 +3,8 @@
 
 #include <llosl/ShaderGroup.h>
 
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/DerivedTypes.h>
 #include <llvm/IR/Metadata.h>
 #include <llvm/IR/Module.h>
 
@@ -26,11 +28,46 @@ ShaderGroup::ShaderGroup(BuilderImpl& builder)
     d_main_function_md.reset(
         llvm::ValueAsMetadata::get(shading_system.get_group_main_function(shader_group.get())));
 
+    int parameter_count = 0;
+    OSL::ustring *parameter_names = nullptr;
+    OSL::TypeDesc *parameter_types = nullptr;
+
+    shading_system.getattribute(shader_group.get(), "num_userdata", parameter_count);
+    shading_system.getattribute(shader_group.get(), "userdata_names", OSL::TypeDesc::PTR, &parameter_names);
+    shading_system.getattribute(shader_group.get(), "userdata_types", OSL::TypeDesc::PTR, &parameter_types);
+
+    d_parameter_count = parameter_count;
+    d_parameters = std::allocator<Parameter>().allocate(d_parameter_count);
+
+    auto parameter = d_parameters;
+    std::vector<llvm::Type *> parameter_ll_types;
+
+    for (unsigned index = 0; parameter_count > 0; --parameter_count, ++index) {
+        new (parameter++) Parameter(index, *parameter_names++, *parameter_types++, this);
+        parameter_ll_types.push_back(d_data_type->getStructElementType(index));
+    }
+
+    auto& ll_context = d_context->getLLContext();
+
+    d_parameters_type = llvm::StructType::get(ll_context, parameter_ll_types);
+
+    std::vector<llvm::Metadata *> mds;
+    std::transform(
+        d_parameters, d_parameters + d_parameter_count,
+        std::back_inserter(mds),
+            [](auto& parameter) -> llvm::Metadata * {
+                return parameter.d_md.get();
+        });
+
+    d_parameters_md.reset(
+        llvm::MDTuple::get(ll_context, mds));
+
     d_md.reset(
-      llvm::MDTuple::get(d_context->getLLContext(), {
-	      d_init_function_md.get(),
-	      d_main_function_md.get()
-	  }));
+        llvm::MDTuple::get(ll_context, {
+            d_init_function_md.get(),
+            d_main_function_md.get(),
+            d_parameters_md.get()
+        }));
 
     auto shadergroups_md = d_module->getOrInsertNamedMetadata("llosl.shadergroups");
     shadergroups_md->addOperand(d_md.get());
@@ -40,6 +77,14 @@ ShaderGroup::~ShaderGroup() {
     if (d_context) {
 	    d_context->removeShaderGroup(this);
     }
+
+    std::for_each(
+        d_parameters, d_parameters+d_parameter_count,
+        [](auto& parameter) -> void {
+	        parameter.~Parameter();
+        });
+
+    std::allocator<Parameter>().deallocate(d_parameters, d_parameter_count);
 }
 
 void
@@ -64,5 +109,68 @@ ShaderGroup::main_function() const {
       : nullptr;
 }
 
+ShaderGroup::Parameter::Parameter(unsigned index, const OSL::ustring& name, const OSL::TypeDesc& type, ShaderGroup *parent)
+: d_parent(parent) {
+    auto& ll_context = d_parent->module()->getContext();
+
+    d_md.reset(
+        llvm::MDTuple::get(
+        ll_context, {
+            llvm::ConstantAsMetadata::get(
+                llvm::ConstantInt::get(ll_context, llvm::APInt(32, index, true))),
+            llvm::MDString::get(ll_context, "llosl.parameter_name"),
+            llvm::MDString::get(ll_context, name.c_str()),
+            llvm::MDString::get(ll_context, "llosl.type"),
+            llvm::ConstantAsMetadata::get(
+                llvm::ConstantInt::get(ll_context, llvm::APInt(8, type.basetype, true))),
+            llvm::ConstantAsMetadata::get(
+                llvm::ConstantInt::get(ll_context, llvm::APInt(8, type.aggregate, true))),
+            llvm::ConstantAsMetadata::get(
+                llvm::ConstantInt::get(ll_context, llvm::APInt(8, type.vecsemantics, true))),
+            llvm::ConstantAsMetadata::get(
+                llvm::ConstantInt::get(ll_context, llvm::APInt(32, type.arraylen, true)))
+        }));
+}
+
+unsigned
+ShaderGroup::Parameter::index() const {
+    auto md = llvm::mdconst::dyn_extract<llvm::ConstantInt>(d_md->getOperand(0).get());
+    assert(md);
+
+	return md->getZExtValue();
+}
+
+llvm::StringRef
+ShaderGroup::Parameter::name() const {
+    return llvm::cast<llvm::MDString>(d_md->getOperand(2).get())->
+	    getString();
+}
+
+llvm::Type *
+ShaderGroup::Parameter::llvm_type() const {
+    return llvm::cast<llvm::StructType>(d_parent->data_type())->getTypeAtIndex(index());
+}
+
+OIIO::TypeDesc
+ShaderGroup::Parameter::osl_type() const {
+    auto basetype_md = llvm::mdconst::dyn_extract<llvm::ConstantInt>(d_md->getOperand(4).get());
+    assert(basetype_md);
+
+    auto aggregate_md = llvm::mdconst::dyn_extract<llvm::ConstantInt>(d_md->getOperand(5).get());
+    assert(aggregate_md);
+
+    auto vecsemantics_md = llvm::mdconst::dyn_extract<llvm::ConstantInt>(d_md->getOperand(6).get());
+    assert(vecsemantics_md);
+
+    auto arraylen_md = llvm::mdconst::dyn_extract<llvm::ConstantInt>(d_md->getOperand(7).get());
+    assert(arraylen_md);
+
+    return OIIO::TypeDesc(
+        static_cast<OIIO::TypeDesc::BASETYPE>(basetype_md->getZExtValue()),
+        static_cast<OIIO::TypeDesc::AGGREGATE>(aggregate_md->getZExtValue()),
+        static_cast<OIIO::TypeDesc::VECSEMANTICS>(vecsemantics_md->getZExtValue()),
+        static_cast<unsigned>(arraylen_md->getZExtValue())
+    );
+}
 
 } // End namespace llosl
