2b1081c50e4fcc68aaf8ac85506a54dc2a50cd0f
Alex Betts <alex.betts@gmail.com>
2019-10-25 16:54:23 -0400

    WIP codegen

--- lib/Builder.cpp
+++ lib/Builder.cpp
@@ -7,6 +7,10 @@
 
 #include <OSL/oslexec.h>
 
+#include <runtimeoptimize.h>
+#include <osl_pvt.h>
+#include <oslexec_pvt.h>
+
 #include <utility>
 
 using namespace LLOSLUtil;
@@ -14,7 +18,7 @@ using namespace LLOSLUtil;
 namespace llosl {
 
 namespace {
-    
+
 struct ErrorCategory : std::error_category {
     static const ErrorCategory& Get() {
 	static ErrorCategory s_error_category;
@@ -143,15 +147,31 @@ BuilderImpl::Finalize() {
 
   auto osl_error_scope = d_context->enterOSLErrorScope();
 
-  auto& shading_system = d_context->getShadingSystem();
-
-  shading_system.optimize_group(d_shader_group.get(), d_context->getShadingContext());
+  OSL::pvt::RuntimeOptimizer rop(d_context->getShadingContext()->shadingsys(), *d_shader_group, nullptr);
+  rop.run();
 
   auto error = osl_error_scope.takeError();
 
   if (error) {
       d_state = State::kInvalidError;
       return std::move(error);
+  }
+
+  // Satisfy the invariants of OSL::ShaderGroup's destructor:
+  for (int i = 0, n = d_shader_group->nlayers(); i < n; ++i) {
+    auto layer = d_shader_group->layer(i);
+
+    // We no longer needs ops and args -- create empty vectors and
+    // swap with the ones in the instance.
+    OpcodeVec emptyops;
+    layer->ops().swap (emptyops);
+    std::vector<int> emptyargs;
+    layer->args().swap (emptyargs);
+    if (layer->unused()) {
+        // If we'll never use the layer, we don't need the syms at all
+        SymbolVec nosyms;
+        std::swap (layer->symbols(), nosyms);
+    }
   }
 
   return llvm::Expected<std::unique_ptr<ShaderGroup> >(std::unique_ptr<ShaderGroup>(new ShaderGroup(*this)));
